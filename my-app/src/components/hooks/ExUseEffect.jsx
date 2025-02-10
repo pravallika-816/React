f49679388ba64e20f7995ace67f22f0cb9cbc4fa
pravallika-816 <bslpravallika1804@gmail.com>
2025-02-10 10:29:37 +0530

    ReactCodes

--- my-app/src/components/hooks/ExUseEffect.jsx
+++ my-app/src/components/hooks/ExUseEffect.jsx
@@ -1,19 +1,39 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 
 const ExUseEffect = () => {
-    const [count, setCount]=useState(0);
-    useEffect(()=>{
-        // setCount(1000);
-        document.title="MRU"
-        setTimeout(()=>{
-            setCount(1000);
-        },4000)
-    })
+    // useEffect(()=>{
+    //     setCount(1000);
+    //     document.title="MRU"
+    //     setTimeout(()=>{
+    //         setCount(1000);
+    //     },4000)
+    // })
+    
+    const [user, setUser] = useState([]);  
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(data => {
+                setUser(data);
+            })
+            .catch(error => console.log('err'));
+    }, []);
+
     return (
         <div>ExUseEffect
-            <h1>{count}</h1>     
+            {
+                user.map((user, i) => {
+                    return (
+                        <React.Fragment key={i}>
+                            <h2>{user.name}</h2>
+                            <h4>{user.email}</h4>
+                        </React.Fragment>
+                    );
+                })
+            }
         </div>
-    )
+    );
 }
 
-export default ExUseEffect
+export default ExUseEffect;
